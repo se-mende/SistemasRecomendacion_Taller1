19762ff4a8be7216a9948596d11577e137d02d3b
Juan Jose Villegas <juan.villegas@rappi.com>
2020-03-05 15:28:54 -0500

    WIP: Building views

--- recommender/core/models.py
+++ recommender/core/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class UserModel(models.Model):
+    name = models.CharField('Nombre', max_length=60)
--- recommender/core/views.py
+++ recommender/core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.edit import FormView
 from django.views.generic.list import ListView
 from .forms import SearchUserForm
+from django.urls import reverse
 
 def index(request):
 
@@ -14,10 +15,10 @@ def index(request):
 class SearchUserFormView(FormView):
     template_name = 'index.html'
     form_class = SearchUserForm
-    success_url = '/results/'
     
-    # def get_success_url(self):
-    #     return '/'
+    def get_success_url(self):
+        query = self.request.GET.get('query')
+        return '%s?query=%s' %  (reverse('search_results'), query)
 
 class UserSearchResultsListView(ListView):
     template_name = 'user_search_results.html'
