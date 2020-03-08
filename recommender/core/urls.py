4e49396b86a3af9e89e27027e99d3842ca1d6a9a
Juan Jose Villegas <juan.villegas@rappi.com>
2020-03-08 16:01:00 -0500

    Fixed rating model

--- recommender/core/urls.py
+++ recommender/core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.SearchUserFormView.as_view(), name='index'),
+    path('', views.HomeView.as_view(), name='index'),
     path('results/', views.UserSearchResultsListView.as_view(), name='search_results')
 ]
